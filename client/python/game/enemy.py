9f8dbb822aefdb0e1c38a11a114c4674ea75650b
sizuku0124 <09hakuto0124@gmail.com>
2026-03-06 17:23:33 +0900

    feat:バトルシステム用エネミー実装

---
+++ client/python/game/enemy.py
@@ -0,0 +1,65 @@
+class enemy:
+    def __init__(self, name, max_hp, hp, mp, attack, defense, agility):
+        self._name = name
+        self._max_hp = max_hp
+        self._hp = hp
+        self._mp = mp
+        self._attack = attack
+        self._defense = defense
+        self._agility = agility
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def max_hp(self):
+        return self._max_hp
+
+    @property
+    def hp(self):
+        return self._hp
+
+    @property
+    def mp(self):
+        return self._mp
+
+    @property
+    def attack(self):
+        return self._attack
+
+    @property
+    def defense(self):
+        return self._defense
+
+    @property
+    def agility(self):
+        return self._agility
+
+    @name.setter
+    def name(self, value):
+        self._name = value
+
+    @max_hp.setter
+    def max_hp(self, value):
+        self._max_hp = max(0, value)
+
+    @hp.setter
+    def hp(self, value):
+        self._hp = max(0, value)
+
+    @mp.setter
+    def mp(self, value):
+        self._mp = max(0, value)
+
+    @attack.setter
+    def attack(self, value):
+        self._attack = value
+
+    @defense.setter
+    def defense(self, value):
+        self._defense = value
+
+    @agility.setter
+    def agility(self, value):
+        self._agility = value
